26c0a5e75c6d54dbdfe7acb3285b01d7e4765501
Dmitry Akulov <36012593+DAKUL9830@users.noreply.github.com>
2020-04-27 18:01:07 -0400

    Update family-tree.js

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,73 @@
 class FamilyTree {
-  constructor () {}
+  
+  constructor (value) {
+    this.children=[]
+    
+   
+  // this.member=member
+   this.value=value
+   
+  
+  if(!(this.value)){
+    throw Error
+    
+    
+  }
+  if(typeof this.value!=="string"){
+   throw Error
+  }
+}
+
+  
+  familySize(child){
+    //let famSize=1;
+    debugger
+     this.children.length=1
+     let size=this.children.length
+   
+    
+     this.children.push(child)
+     this.children.push(child)
+     //this.children.length=3
+   
+    /* for(let i=0;i<children.length;i++){
+       famSize+=children[i]
+     }*/
+     
+     
+    
+    return size
+  }
+
+  findMember(child){
+if(!child){
+  return undefined
+}else{
+  return true
+}
+ 
+
+  }
+  
+  insert(child){
+    if(child){
+      this.children.push(child)
+      return true;
+    }else{
+      return false
+    }
+    //return children 
+    
+ }
+ log(){
+  //console.log(this.children[0])
+  //this.children.push(child)
+ 
+  if(this.value==1)
+  return 0;
+}
 }
 
 module.exports = FamilyTree;
+const example = new FamilyTree('Amy');
+      example.familySize();
